c2ec8301f76dd87303da1fc45182b72a4845f193
Dan DeAraujo <daniel.dearaujo@d2l.com>
2019-09-20 16:00:15 -0400

    revert 'update copyright date'

--- doRequest.php
+++ doRequest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2019 Desire2Learn Inc.
+ * Copyright (c) 2012 Desire2Learn Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
