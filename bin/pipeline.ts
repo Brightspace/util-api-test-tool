a97711be44b2c13af2cf728aa173774f6070f3c5
Richard Martin <Richard.Martin@D2L.com>
2020-12-22 13:33:31 -0500

    Remove region and account information.

--- bin/pipeline.ts
+++ bin/pipeline.ts
@@ -1,23 +1,20 @@
-import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { AppStack } from '../lib/app-stack';
 import { SetupStack } from '../lib/setup-stack'
 
 const app = new cdk.App();
-const region = "us-west-1";
-const account = '084374970894';
 
 new SetupStack(app, "ApiTestToolSetupStack", {
     env: { 
-        account: account, 
-        region: region 
+        account: process.env.CDK_DEFAULT_ACCOUNT, 
+        region: process.env.CDK_DEFAULT_REGION
     }
 });
   
 const appStack = new AppStack(app, "ApiTestToolAppStack", {
     env: { 
-        account: account, 
-        region: region
+        account: process.env.CDK_DEFAULT_ACCOUNT, 
+        region: process.env.CDK_DEFAULT_REGION
     }
 });
 
