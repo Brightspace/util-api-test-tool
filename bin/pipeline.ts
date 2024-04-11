1f8e6713b32262783c4134212b829061318005aa
Richard Martin <Richard.Martin@D2L.com>
2024-04-11 10:27:42 -0400

    Reducing maxAzs to 1, and fargate tasks to 1

--- bin/pipeline.ts
+++ bin/pipeline.ts
@@ -1,37 +1,37 @@
-import * as cdk from 'aws-cdk-lib';
-import { AppStack, AppStackProps } from '../lib/app-stack';
-import { SetupStack } from '../lib/setup-stack';
+import * as cdk from "aws-cdk-lib";
+import { AppStack, AppStackProps } from "../lib/app-stack";
+import { SetupStack } from "../lib/setup-stack";
 
 const prdAppStackProps: AppStackProps = {
-    env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: "us-east-1"
-    },
-    hostedZoneDomainName: "desire2learnvalence.com",
-    certificateDomain: "apitesttool.desire2learnvalence.com",
-    fargateDomainName: "apitesttool.desire2learnvalence.com",
-    elbLogTargetEnvironment: "prd",
-    imageTag: process.env.IMAGE_TAG
-}
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: "us-east-1",
+  },
+  hostedZoneDomainName: "desire2learnvalence.com",
+  certificateDomain: "apitesttool.desire2learnvalence.com",
+  fargateDomainName: "apitesttool.desire2learnvalence.com",
+  elbLogTargetEnvironment: "prd",
+  imageTag: process.env.IMAGE_TAG,
+};
 
 const devAppStackProps: AppStackProps = {
-    env: {
+  env: {
         account: "111111111111",
-        region: "ca-central-1",
-    },
-    hostedZoneDomainName: "lti.dev.brightspace.com",
-    certificateDomain: "apitesttool.lti.dev.brightspace.com",
-    fargateDomainName: "apitesttool.lti.dev.brightspace.com",
-    elbLogTargetEnvironment: "dev",
-    imageTag: "test1"
-}
+    region: "ca-central-1",
+  },
+  hostedZoneDomainName: "lti.dev.brightspace.com",
+  certificateDomain: "apitesttool.lti.dev.brightspace.com",
+  fargateDomainName: "apitesttool.lti.dev.brightspace.com",
+  elbLogTargetEnvironment: "dev",
+  imageTag: "test1",
+};
 
 const app = new cdk.App();
 
-new SetupStack(app, "ApiTestToolSetupStack", prdAppStackProps );
-new AppStack(app, "ApiTestToolAppStack", prdAppStackProps );
+new SetupStack(app, "ApiTestToolSetupStack", prdAppStackProps);
+new AppStack(app, "ApiTestToolAppStack", prdAppStackProps);
 
-new SetupStack(app, "ApiTestToolSetupStack-Dev", devAppStackProps );
-new AppStack(app, "ApiTestToolAppStack-Dev", devAppStackProps );
+new SetupStack(app, "ApiTestToolSetupStack-Dev", devAppStackProps);
+new AppStack(app, "ApiTestToolAppStack-Dev", devAppStackProps);
 
 app.synth();
